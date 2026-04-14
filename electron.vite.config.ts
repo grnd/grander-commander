a891c4e78aa9c9d830c5b8eadebc48c4a6c47443
Danny Grander <danny.grander@gmail.com>
2026-04-14 10:01:49 +0100

    fix(build): add @shared/@main/@renderer aliases to vite config

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -5,15 +5,32 @@ import { resolve } from 'path';
 export default defineConfig({
   main: {
     plugins: [externalizeDepsPlugin()],
+    resolve: {
+      alias: {
+        '@shared': resolve('src/shared'),
+        '@main': resolve('src/main'),
+      },
+    },
     build: { rollupOptions: { input: resolve('src/main/main.ts') } },
   },
   preload: {
     plugins: [externalizeDepsPlugin()],
+    resolve: {
+      alias: {
+        '@shared': resolve('src/shared'),
+      },
+    },
     build: { rollupOptions: { input: resolve('src/preload/preload.ts') } },
   },
   renderer: {
     root: resolve('src/renderer'),
     plugins: [react()],
+    resolve: {
+      alias: {
+        '@shared': resolve('src/shared'),
+        '@renderer': resolve('src/renderer'),
+      },
+    },
     build: { rollupOptions: { input: resolve('src/renderer/index.html') } },
   },
 });
